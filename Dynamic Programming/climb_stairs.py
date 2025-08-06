18f79c48a990d3c8a4576cd91b472ec61f179d07
roshan13ghimire <roshan13ghimire@gmail.com>
2025-08-06 12:15:46 -0400

    c

---
+++ Dynamic Programming/climb_stairs.py
@@ -0,0 +1,9 @@
+#climb_stairs
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return 1
+        a,b = 1,1
+        for i in range(2,n+1):
+            a,b = b, a+b
+        return b
